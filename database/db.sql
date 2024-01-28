47464f2d43e546ef9ce4b6119649eb435f853005
youssef abbas <youssef.ahmedabbas99@gmail.com>
2024-01-28 13:41:51 +0200

    databse sql

---
+++ database/db.sql
@@ -0,0 +1,26 @@
+-- CREATE DATABASES ADD
+
+CREATE DATABASE IF NOT EXISTS api_post;
+
+USE api_post;
+
+-- create user ADD
+-- ID , NAME , EMAIL , PASSWORD , last login
+
+CREATE TABLE USER (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+-- create post
+-- ID , title , content , image  , user_id
+
+CREATE TABLE POST (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    content text ,
+    user_id BIGINT,
+    FOREIGN KEY(user_id) REFERENCES USER(id) ON DELETE SET NULL
+);
+
